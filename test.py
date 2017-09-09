ca5dc2755f41f000f301e5ecf547ef1fbbccbbd0
Pengkun Yang <pyang14@illinois.edu>
2017-09-09 00:18:39 -0500

    Add WordCloud class, incorporating parameters, add one font file, add passwords file.

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 from wordcloud import *
 
-text = read_file('tres_leches.txt')
-words = text_to_words(text)
-words = pre_process(words)
-freq = words_to_freq(words)
-plot_from_freq(freq)
-# print(words)
-# for k in freq:
-#     print(k,freq[k])
+
+wc = WordCloud(passwords='passwords')
+
+wc.plot('cheesecake.txt')
--- wordcloud.py
+++ wordcloud.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 import math
 import random
 from tools import Counter, Box
+import os
 
 
-STOPWORDS = set([x.strip() for x in open('stopwords').read().split('\n')])
-PASSWORDS = set([x.strip() for x in open('passwords').read().split('\n')])
 
 
         
@@ -63,83 +62,141 @@ def tokenize(word):
     return word
 
 
-def pre_process(words):
-    '''
-    words pre-processing
-
-    R text process refs: https://cran.r-project.org/web/packages/tm/tm.pdf
-    Python package: NLTK (but a heavy package)
-    '''
-
-    words = [tokenize(word) for word in words] # to lower case
-    # words = [word for word in words if word not in STOPWORDS] # remove STOPWORDS
-    words = [word for word in words if word in PASSWORDS] # only allow PASSWORDS
-    
-    return words
 
 
 
 
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
-def plot_from_freq(freq):
-    freq = sorted(freq.items(), key=operator.itemgetter(1), reverse=True)
-    freq = freq[:100]
-    max_freq = float(freq[0][1])
-    
-    max_size = 100
-    min_size = 10
-    
-    width = 1000
-    height = 1000
-    
-    img_color = Image.new("RGB", (width, height))
-    draw = ImageDraw.Draw(img_color)
-    
-    draw.rectangle(((0, 0), (width, height)), fill="white")
-    a = Canvas(width,height)
-    rank = 1
-    for word, f in freq:
-        f_normalize = f/max_freq # normalize frequency
-        size = (max_size - min_size) * f_normalize + min_size
-        
-        # f_normalize = rank -1 # draw by rank
-        # size = max_size - (max_size - min_size)/100 * f_normalize
-        rank = rank + 1
-        # get (lx,ly) from (word, size, [font])
-        # print(lx,ly)
-        font = ImageFont.truetype("DroidSansMono.ttf", int(size))
-        lx,ly = draw.textsize(word, font = font)
-
-        # find (x,y,rotation) from (lx,ly)
-        x,y,rotation = a.find_position(lx,ly)
-        print(x,y,lx,ly,rotation)
-
-        if x<0:
-            continue
-        # plot word on x,y with rotation    
-        if (not rotation):
-            draw.text((x,y), word, font = font, fill = (random.randint(0,255),random.randint(0,255),random.randint(0,255),0))
-            a.add(Box(x,y,lx,ly))
-        else:
-            font = ImageFont.TransposedFont(font, orientation=Image.ROTATE_90)
-            draw.text((x,y), word, font = font, fill = (random.randint(0,255),random.randint(0,255),random.randint(0,255),0))
-            a.add(Box(x,y,ly,lx))
-            
-    img_color.save("tres_leches.jpg","JPEG")
-
-
-
 class WordCloud(object):
-    def __init__(self):
-        return
+    def __init__(self,
+                 font_path="DroidSansMono.ttf",
+                 max_words=100,
+                 max_size=100, min_size=10,
+                 width=1000, height=1000,
+                 save_file="test.jpg",
+                 stopwords="stopwords", passwords=None,
+                 verbose=False):
+        """
+        Object to generate word cloud
+
+        Args:
+        font_path: string
+        path to the font to be used
+
+        max_words: int
+        max number of words to shown in word cloud
+
+        max_size, min_size: int
+        maximum and minimum font size
+
+        width, height: int
+        size of the entire canvas
+
+        save_file: string
+        filename of the saved picture
+
+        stopwords: string
+        name of file that contains STOPWORDS
+
+        passwords: string
+        name of file that contains PASSWORDS (only use those words)
+
+        verbose: bool
+        if verbose=True, will print on screen
+        
+        """
+        self.font_path = font_path
+        self.max_words = max_words
+        self.max_size = max_size
+        self.min_size = min_size
+        self.width = width
+        self.height = height
+        self.save_file = save_file
+
+
+        self.stopwords = stopwords
+        self.passwords = passwords
+        self.verbose = verbose
+
+
+        
+    def pre_process(self,words):
+        '''
+        words pre-processing
+        
+        R text process refs: https://cran.r-project.org/web/packages/tm/tm.pdf
+        Python package: NLTK (but a heavy package)
+        '''
+
+        words = [tokenize(word) for word in words] 
+        if self.stopwords != None:
+            STOPWORDS = set([x.strip() for x in open(self.stopwords).read().split('\n')])
+            words = [word for word in words if word not in STOPWORDS] # remove STOPWORDS
+        if self.passwords != None:
+            PASSWORDS = set([x.strip() for x in open(self.passwords).read().split('\n')])
+            words = [word for word in words if word in PASSWORDS] # only allow PASSWORDS
+    
+        return words
 
     
-    def plot(freq):
+    def plot(self,filename):
+        text = read_file(filename)
+        words = text_to_words(text)
+        words = self.pre_process(words)
+        freq = words_to_freq(words)
+        self.plot_from_freq(freq)
+
+
+        
+    def plot_from_freq(self,freq):
         '''
         
         R world cloud refs: https://cran.r-project.org/web/packages/wordcloud/wordcloud.pdf
         
         '''
+        if len(freq) <= 0:
+            raise ValueError("We need at least 1 word to plot a word cloud, "
+                             "got %d." % len(freq))
+        
+        freq = sorted(freq.items(), key=operator.itemgetter(1), reverse=True)
+        freq = freq[:self.max_words]
+        max_freq = float(freq[0][1])
+
+
+        canvas = Canvas(self.width,self.height)                           # an empty canvas
+        img_color = Image.new("RGB", (self.width, self.height))           # handler to a colored image
+        draw = ImageDraw.Draw(img_color)                                  # tool to draw on img_colord
+        draw.rectangle(((0, 0), (self.width, self.height)), fill="white") # initial fill with white
+        
+        for word, f in freq:
+            f_normalize = f/max_freq # normalize frequency
+
+            # compute word size. Possibly other ways, for example, using relative_scaling
+            size = (self.max_size - self.min_size) * f_normalize + self.min_size
+        
+            # get (lx,ly) from (word, size, [font])
+            font = ImageFont.truetype(self.font_path, int(size))
+            lx,ly = draw.textsize(word, font = font)
+
+            # find (x,y,rotation) from (lx,ly). return x=-1 if failed
+            x,y,rotation = canvas.find_position(lx,ly)
+
+            if self.verbose:
+                print(x,y,lx,ly,rotation)
+
+            if x<0:
+                continue
+            # plot word on x,y with rotation    
+            if (not rotation):
+                draw.text((x,y), word, font = font, fill = (random.randint(0,255),random.randint(0,255),random.randint(0,255),0))
+                canvas.add(Box(x,y,lx,ly))
+            else:
+                font = ImageFont.TransposedFont(font, orientation=Image.ROTATE_90)
+                draw.text((x,y), word, font = font, fill = (random.randint(0,255),random.randint(0,255),random.randint(0,255),0))
+                canvas.add(Box(x,y,ly,lx))
+            
+        img_color.save(self.save_file)
+
 
 
 def read_file(filename):
